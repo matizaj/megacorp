f0765d942a5c05f2631473bb97cd61af9a96369f
Mateusz <matzaj101@gmail.com>
2025-10-30 22:19:56 +0100

    K
    L
    
    M

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -8,3 +8,6 @@ printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
 grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
 echo "========= PHONE NUMBER SCAN COMPLETE ========="
+echo dupa
+duopa2
+duopa3
